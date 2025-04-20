3595e3cd01bf7d56de9c911aa093c467f9eec7a1
rylan-dunning <brdunning14@gmail.com>
2025-04-19 19:24:55 -0600

    added tester user seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,10 +21,19 @@ class UserSeeder extends Seeder
                 'name' => 'Rylan Dunning',
                 'email' => 'brdunning14@gmail.com',
                 'email_verified_at' => null,
+                'password' => bcrypt(value: 'Clam1921!'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Tester',
+                'email' => 'testEmail@gmail.com',
+                'email_verified_at' => null,
                 'password' => bcrypt(value: 'password'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
-            ]
+            ],
+
         ];
         User::insert($users);
     }
